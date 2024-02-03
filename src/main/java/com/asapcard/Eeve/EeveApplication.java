b20c00c246c82cb6914da85429164735fc7c9506
Daniela Xavier <danielamxsoares@gmail.com>
2024-02-03 10:08:07 -0300

    feat: criar estrutura de projetos

---
+++ src/main/java/com/asapcard/Eeve/EeveApplication.java
@@ -0,0 +1,13 @@
+package com.asapcard.Eeve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EeveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EeveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asapcard/Eeve/EeveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asapcard.Eeve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
