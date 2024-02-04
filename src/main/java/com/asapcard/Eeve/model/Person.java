77d25c02734f207c0f005bd6bf29ef694cf03242
johnnataa <johnnata_@outlook.com>
2024-02-04 15:37:27 -0300

    feat: continuação do commit anterior

--- src/main/java/com/asapcard/Eeve/model/Person.java
+++ src/main/java/com/asapcard/Eeve/model/Person.java
@@ -18,5 +18,6 @@ public class Person {
 	private String name;
 
 	private Integer age;
-	
+
+
 }
--- src/main/java/com/asapcard/Eeve/service/ListenerService.java
+++ src/main/java/com/asapcard/Eeve/service/ListenerService.java
@@ -1,66 +1,51 @@
 package com.asapcard.Eeve.service;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Optional;
 import java.util.UUID;
 
-import org.apache.el.stream.Optional;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.asapcard.Eeve.model.JsonTransaction;
 import com.asapcard.Eeve.model.Person;
-import com.asapcard.Eeve.model.Transaction;
 import com.asapcard.Eeve.repository.InstallmentRepository;
 import com.asapcard.Eeve.repository.PersonRepository;
 import com.asapcard.Eeve.repository.TransactionRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 @Service
 public class ListenerService {
-	
-	@Autowired
-	private PersonRepository personRepository;
-	
-	@Autowired
-	private InstallmentRepository installmentRepository;
-	
-	@Autowired
-	private TransactionRepository transactionRepository;
 
-	
-	private final ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private PersonRepository personRepository;
 
-    @RabbitListener(queues = "queue-a") 
+    @Autowired
+    private InstallmentRepository installmentRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @RabbitListener(queues = "queue-a")
     public void listen(String message) {
-
         try {
-           
             JsonNode jsonNode = objectMapper.readTree(message);
 
             Person person = new Person();
             person.setId(jsonNode.get("id").asLong());
             person.setName(jsonNode.get("nome").asText());
             person.setAge(jsonNode.get("idade").asInt());
-            
-            personRepository.save(person);
-            
-            java.util.Optional<Person> personFinded = personRepository.findById(person.getId());
+
+            java.util.Optional<Person> personFound = personRepository.findById(person.getId());
             UUID uuid = UUID.randomUUID();
 
-            if (!personFinded.isPresent()) { 
+            if (!personFound.isPresent()) {
                 personRepository.save(person);
             }
-            
-            
         } catch (Exception e) {
             e.printStackTrace();
         }
- }}
-
+    }
+}
